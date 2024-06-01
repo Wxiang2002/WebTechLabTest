94d125d88e4ae147d598814ac66cf22748b28490
Wiang2002 <billysoo2002@gmail.com>
2024-06-01 18:06:30 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+ <div class="container">
+  <NavBarComponent />
+  <ContentComponent />
+  </div>
+</template>
+
+<script>
+import ContentComponent from './components/ContentComponent.vue'
+import NavBarComponent from './components/NavBarComponent.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ContentComponent,
+    NavBarComponent
+  }
+}
+</script>
+
+<style scoped>@import './assets/styles.css';</style>
---
+++ src/components/NavBarComponent.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+    name: 'NavBarComponent'
+}
+</script>
+
+<style scoped>
+@import "../assets/styles.css";
+</style>
+
+<template>
+ <div class="navbar">
+            <div class="icon">
+                <h2 class="logo">W.Tech.</h2>
+            </div>
+            <div class="menu">
+                <ul>
+                    <li><a href="#">HOME</a></li>
+                    <li><a href="#">ABOUT</a></li>
+                    <li><a href="#">COURSE MATERIALS</a></li>
+                    <li><a href="#">ASSESSMENT</a></li>
+                    <li><a href="#">CONTACT US</a></li>
+                </ul>
+            </div>
+            <div class="search">
+                <input class="srch" type="search" name="" placeholder="Type To Find">
+                <a href="#"> <button class="btn">Find</button></a>
+            </div>
+        </div>
+        </template>
